8c0642a19b353f26900de2cb466b142398b43abb
Leslie-Xy <540049476@qq.com>
2025-07-04 01:53:34 +0800

    pr检查优化

--- internal/op/label_file_binding.go
+++ internal/op/label_file_binding.go
@@ -1,6 +1,7 @@
 package op
 
 import (
+	"fmt"
 	"github.com/alist-org/alist/v3/internal/db"
 	"github.com/alist-org/alist/v3/internal/model"
 	"github.com/pkg/errors"
@@ -25,8 +26,11 @@ func GetLabelByFileName(userId uint, fileName string) ([]model.Label, error) {
 func CreateLabelFileBinDing(labelIds, fileName string, userId uint) error {
 	labelMap := strings.Split(labelIds, ",")
 	for _, value := range labelMap {
-		labelId, _ := strconv.ParseUint(value, 10, 64)
-		if err := db.CreateLabelFileBinDing(fileName, uint(labelId), userId); err != nil {
+		labelId, err := strconv.ParseUint(value, 10, 64)
+		if err != nil {
+			return fmt.Errorf("invalid label ID '%s': %v", value, err)
+		}
+		if err = db.CreateLabelFileBinDing(fileName, uint(labelId), userId); err != nil {
 			return errors.WithMessage(err, "failed labels in database")
 		}
 	}
